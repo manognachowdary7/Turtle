1c0dd31b9133f044e8362bcf02869c2ac82fce36
V.MANOGNA <65440296+manognachowdary7@users.noreply.github.com>
2021-10-22 14:01:31 +0530

    Add files via upload

---
+++ program7.py
@@ -0,0 +1,33 @@
+import turtle
+import time
+turtle.color("orange")
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+
+turtle.color("orange")
+turtle.left(20)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+
+turtle.color("orange")
+turtle.left(30)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+
+turtle.color("orange")
+turtle.left(40)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(50)
+turtle.hideturtle()
